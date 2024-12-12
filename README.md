45abe6287d35da586123eb7afd1f75ed6ce9a4c1
Jiří Bělský <jiribelsky@hotmail.com>
2024-12-12 19:00:32 +0000

    Git history reset

---
+++ README.md
@@ -0,0 +1,4 @@
+# FRAP
+Free Responsive Admin Panel   
+
+Backup of really old admin panel i coded in my free time. Open, read or use this code at your own risk ;)
---
+++ webpack.mix.js
@@ -0,0 +1,5 @@
+const mix = require('laravel-mix');
+
+mix.copy('./node_modules/font-awesome/css/', './css/');
+
+mix.copy('./node_modules/font-awesome/fonts/', './fonts/');
